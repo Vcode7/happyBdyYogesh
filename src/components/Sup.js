b823fb0074db52d80114513dcf4b13c391d5c720
Your Name <you@example.com>
2021-09-11 04:31:11 +0000

    update

--- src/components/Sup.js
+++ src/components/Sup.js
@@ -1,47 +1,27 @@
 import React from 'react'
 import './sup.css'
 
-export const Sup = () => {    
+export const Sup = () => {
 
-    function handlebtnclick(n) {
-        const uppermodal = document.querySelector('#uppermodal')
-        const modal = document.querySelector('.modal')
-        const lowermodal = document.querySelector('#lowermodal')
-        if(n === 0){
-            modal.style.display = 'flex'
-            setTimeout(() => {
-                uppermodal.style.left = '0vw'
-                lowermodal.style.right = '0vw'
-            }, 500);
-        }
-        else{
-            uppermodal.style.left = '100vw'
-            lowermodal.style.right = '100vw'
-            setTimeout(() => {
-                modal.style.display = 'none'
-            }, 500);
-        }
-    }
     return (
         <>
-            <div className="modal" >
-                <button onClick={handlebtnclick(1)} className="cancle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                    </svg>
-                </button>
-                <div id="uppermodal" className="uppermodal modalcase">
-					
+            <div className="modal" tabindex="-1">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
                 </div>
-
-                <div id="lowermodal" className="modalcase">
-					
-                </div>
-
             </div>
-            <button onClick={handlebtnclick(0)} className="modalbtn">
-                click here
-            </button>
         </>
     )
 }
